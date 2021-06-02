e7a13b9fcf754bf1651ba057a9d39bde0145e559
logic-fabric <lowic@logic-fabric.fr>
2021-06-02 15:24:15 +0200

    style: add PlantItem.css

--- src/components/PlantItem.js
+++ src/components/PlantItem.js
@@ -1,17 +1,22 @@
 import React from "react";
 
 import CareScale from "./Carescale";
+import "../styles/PlantItem.css";
 
 class PlantItem extends React.Component {
   render() {
     const { id, name, water, light, cover } = this.props;
     return (
-      <li key={id} className="jh-plant-item">
-        <img className="jh-plant-item__cover" src={cover} alt={name} />
-        <h2 className="jh-plant-item__title">{name}</h2>
-        <div>
-          <CareScale id={id} careType="light" scaleValue={light} />
-          <CareScale id={id} careType="water" scaleValue={water} />
+      <li key={id} className="plant-item">
+        <div className="plant-item__cover">
+          <img src={cover} alt={name} />
+        </div>
+        <div className="plant-item__infos">
+          <h2 className="plant-item__title">{name}</h2>
+          <div className="plant-item__scales">
+            <CareScale id={id} careType="light" scaleValue={light} />
+            <CareScale id={id} careType="water" scaleValue={water} />
+          </div>
         </div>
       </li>
     );
