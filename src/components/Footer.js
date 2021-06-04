e138e6f3d797d544a617653f8c399a8138c5cccb
logic-fabric <lowic@logic-fabric.fr>
2021-06-04 11:43:21 +0200

    style: add Footer.css

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,7 @@
 import { useState } from "react";
 
+import "../styles/Footer.css";
+
 function Footer() {
   const [inputValue, setInputValue] = useState("");
 
@@ -11,23 +13,30 @@ function Footer() {
 
   function handleBlur(e) {
     if (!inputValue.includes("@")) {
-      alert("Attention votre adresse mail doit comporter un @ pour être valide");
+      alert(
+        "Attention votre adresse mail doit comporter un @ pour être valide"
+      );
     }
   }
 
   return (
     <footer>
-      <div>
-        <h2>Jungle House, la maison des amoureux des plantes</h2>
-      </div>
-      <label>Inscrivez-vous à notre newsletter :</label>
+      <h2 className="footer__headline">
+        Jungle House, la maison des amoureux des plantes
+      </h2>
+      <label className="footer__newsletter-label">
+        Inscrivez-vous à notre newsletter
+      </label>
       <input
+        className="footer__newsletter-input"
         placeholder="Votre e-mail"
         onChange={handleInput}
         onBlur={handleBlur}
         value={inputValue}
       />
-      <p>Jungle House 2021, tous droits resérvés</p>
+      <p className="footer__disclaimer">
+        Jungle House 2021, tous droits reservés
+      </p>
     </footer>
   );
 }
